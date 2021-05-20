9aae68aa4c69aad51958e3149379e62e94cad691
Yashnighot <84264138+Yashnighot@users.noreply.github.com>
2021-05-21 00:23:11 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,35 @@
+  <!--footer-->
+  <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-sm-8">
+                    <h5>Our Address</h5>
+                    <address>
+                        1050, Grand Ave <br>
+                        Chester, New York(NY) <br>
+                        USA, 60964 <br>
+                        <i class="fa fa-envelope fa-lg"></i>&nbsp;: <a href="mailto:nighotyash@rknec.edu"> Mail Us</a>
+                    </address>
+                </div>
+                <div class="col-6 col-sm-4 followus">
+                    <div class="text-center">
+                        <h5> Follow Us </h5>
+                        <ul class="list-unstyled">
+                            <li> <a class="btn btn-social-icon" href="https://github.com/Yashnighot"
+                                    target="_blank"><i class="fa fa-github"></i> </a></li>
+                            <li> <a class="btn btn-social-icon btn-twitter" href="https://www.linkedin.com/in/yash-nighot-9a19221b1/"><i
+                                        class="fa fa-linkedin"></i></a></li>
+                            <li> <a class="btn btn-social-icon btn-google"
+                                    href="https://instagram.com/yashnighot09" target="_blank"><i
+                                        class="fa fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-auto">
+                    <p>© Copyright 2021 Basic Banking System by Yash Nighot</p>
+                </div>
+            </div>
+        </div>
+    </footer>
